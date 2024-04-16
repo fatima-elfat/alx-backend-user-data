22767c550e805243454341413c012bf5a641c537
fatima-elfat <ffzelfatehy@gmail.com>
2024-04-17 00:09:24 +0100

    task 5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,6 +2,7 @@
 """
 Route module for the API
 """
+from api.v1.auth.auth import Auth
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
@@ -12,6 +13,10 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = getenv("AUTH_TYPE", "auth")
+if auth_type == "auth":
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -35,6 +40,28 @@ def forbidden(error) -> str:
     Task 2. Error handler: Forbidden.
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def authorization():
+    """
+    Task 5. Request validation!
+    if auth is None, do nothing
+    if request.path is not part of this list
+    ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'],
+    do nothing - you must use the method require_auth from the auth instance
+    if auth.authorization_header returns None, raise  401 - you must use abort
+    if auth.current_user returns None, raise 403 - you must use abort.
+    """
+    if auth:
+        p = ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"]
+        if auth.require_auth(request.path, p):
+            auth_ = auth.authorization_header(request)
+            if auth_ is None:
+                abort(401)
+            user = auth.current_user(request)
+            if user is None:
+                abort(403)
 
 
 if __name__ == "__main__":
